53758ec947266d8eb0dfab2f3f814a21a4274cd5
SanelisiweDiya <sane2725diya@gmail.com>
2023-09-23 15:18:32 +0200

    added unit conversion

--- src/index.js
+++ src/index.js
@@ -38,17 +38,44 @@ function showWeather(response) {
     `"http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png"`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+  let celsiusTemperature = response.data.main.temp;
 }
 function pressSubmit() {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
   searchCity(cityInputElement.value);
 }
+function displayFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
+}
+
+function displayCelsius(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
 function searchCity(city) {
   let apiKey = "f09d3949047ab6c9e3bcaf79cf61f619";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showWeather);
 }
-searchCity("Margate");
+searchCity("Cape Town");
 let Form = document.querySelector(".search-form");
 searchForm.addEventListener("submit", pressSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsius);
